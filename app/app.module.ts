11e2b42493e2b52d015e10c1533658114239986a
Marko Leppänen <marko.leppanen@cimcorp.com>
2016-10-22 15:09:43 +0400

    advanced

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @NgModule({
     imports: [
@@ -9,11 +11,9 @@ import {AppComponent} from "./app.component";
         FormsModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        HeroDetailComponent
     ],
-    bootstrap: [
-        AppComponent
-    ]
+    bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
